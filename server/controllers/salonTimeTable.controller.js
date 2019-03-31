9232f2a517379e74af8b0ecbdf19cb1c69342ba5
iamzee <zeeshan99adeen@gmail.com>
2019-03-31 15:57:59 +0530

    setup list timetable api on server

--- server/controllers/salonTimeTable.controller.js
+++ server/controllers/salonTimeTable.controller.js
@@ -23,4 +23,35 @@ const create = (req, res) => {
   });
 };
 
-module.exports = {create};
+const list = (req, res) => {
+  const {salonId} = req.query;
+
+  sql = `
+    SELECT
+      tt.day,
+      tt.opening_time,
+      tt.closing_time
+    FROM salon_time_table AS tt
+    INNER JOIN salon
+    ON salon.id = tt.salon_id
+    WHERE salon.id = ?
+  `;
+
+  connection.query (sql, parseInt (salonId, 10), function (
+    err,
+    results,
+    fields
+  ) {
+    if (err) {
+      return res.status (400).json ({
+        err,
+      });
+    }
+
+    res.status (200).json ({
+      timeTable: results,
+    });
+  });
+};
+
+module.exports = {create, list};
--- server/routes/salonTimeTable.route.js
+++ server/routes/salonTimeTable.route.js
@@ -1,8 +1,12 @@
 const express = require ('express');
+const authCtrl = require ('../controllers/auth.controller');
 const salonTimeTableCtrl = require ('../controllers/salonTimeTable.controller');
 
 const router = express.Router ();
 
-router.route ('/api/salonTimeTable').post (salonTimeTableCtrl.create);
+router
+  .route ('/api/salonTimeTable')
+  .post (authCtrl.requireSignin, salonTimeTableCtrl.create)
+  .get (authCtrl.requireSignin, salonTimeTableCtrl.list);
 
 module.exports = router;
